d8e618c719f471499a0e1e5fe608c9b6b12f981a
Nicolás Vilurón <viluronnicolas@gmail.com>
2023-06-11 22:12:26 -0300

    Ya lee todo el archivo

--- comprimir/huffman/unzip.go
+++ comprimir/huffman/unzip.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -31,16 +32,32 @@ func GetFromCompacted() (string, *arbol, error) {
 
 	originalLength := int(binary.LittleEndian.Uint32(lengthBytes))
 
-	dataBytes := make([]byte, (originalLength+7)/8)
-	_, err = reader.Read(dataBytes)
+	dataBytes := make([]byte, 1024)
+	var acumulador []byte
+	/* _, err = reader.Read(dataBytes)
 	if err != nil {
-		return "", nil, err
+		return "", err
+	} */
+	for {
+		n, err := reader.Read(dataBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("Error al leer el archivo: ", err)
+				return "", nil, err
+			}
+		}
+		if n == 0 {
+			break
+		}
+		acumulador = append(acumulador, dataBytes...)
 	}
 
 	data := ""
 
 	//Lee los bytes del archivo y los convierte en una cadena binaria
-	for _, byteVal := range dataBytes {
+	for _, byteVal := range acumulador {
 		for j := 7; j >= 0; j-- {
 			if (byteVal >> j & 1) == 1 {
 				builder.WriteByte('1')
@@ -62,6 +79,70 @@ func GetFromCompacted() (string, *arbol, error) {
 	}
 	return data, raiz, nil
 }
+
+// ------- Borrar cuando termine la prueba -------------
+func GetFromCompactedDATA() (string, error) {
+	//Abre el archivo para lectura
+	var builder strings.Builder
+	file, err := os.Open("./comprimir/resultados/comprimidoDATA.huf")
+
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	//Crea un lector de búfer
+	reader := bufio.NewReader(file)
+
+	//Lee la longitud original de los datos del archivo
+	lengthBytes := make([]byte, 4)
+	_, err = reader.Read(lengthBytes)
+	if err != nil {
+		return "", err
+	}
+
+	originalLength := int(binary.LittleEndian.Uint32(lengthBytes))
+
+	dataBytes := make([]byte, 1024)
+	var acumulador []byte
+	/* _, err = reader.Read(dataBytes)
+	if err != nil {
+		return "", err
+	} */
+	for {
+		n, err := reader.Read(dataBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("Error al leer el archivo: ", err)
+				return "", err
+			}
+		}
+		if n == 0 {
+			break
+		}
+		acumulador = append(acumulador, dataBytes...)
+	}
+
+	data := ""
+
+	//Lee los bytes del archivo y los convierte en una cadena binaria
+	for _, byteVal := range acumulador {
+		for j := 7; j >= 0; j-- {
+			if (byteVal >> j & 1) == 1 {
+				builder.WriteByte('1')
+			} else {
+				builder.WriteByte('0')
+			}
+		}
+	}
+	data = builder.String()
+	data = data[:originalLength]
+	return data, nil
+}
+
+// ------ Borrar lo de arriba cuando termine la prueba ----
 
 func cargarArbol(reader *bufio.Reader) (*arbol, error) {
 	flag, err := reader.ReadByte()
